d314d07321b850ac59e8346bdf3ecd1178585c64
Brivan-26 <km_rezazi@esi.dz>
2025-09-06 22:32:08 +0100

    updated zk proof generation scripts

--- script/zk/groth16_proof_generation.bash
+++ script/zk/groth16_proof_generation.bash
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+delete_if_exists() {
+    if [ -e "$1" ]; then
+        echo "Deleting $1..."
+        rm -rf "$1"
+    else
+        echo "$1 does not exist. Skipping..."
+    fi
+}
+
 # Create directories if they don't exist
 mkdir -p zkMetrics/groth16
 
@@ -135,18 +144,23 @@ if [ $? -ne 0 ]; then
 fi
 echo "Verification key exported to verification_key.json."
 
-# Generate the proof and benchmark performance
-echo "Step 13: Generating the proof and benchmarking performance..."
-/usr/bin/time -f "Elapsed Time: %e seconds\nMaximum Memory: %M KB" snarkjs groth16 prove CDRGeneration_final.zkey witness.wtns proof.json public.json 2> zkMetrics/groth16/time_output.txt
+# TRUSTED SETUP DONE, GENERATING PROOF
 
-# Extract proving time and memory usage
-elapsed_time=$(grep "Elapsed Time" zkMetrics/groth16/time_output.txt | awk '{print $3}')
-memory_usage=$(grep "Maximum Memory" zkMetrics/groth16/time_output.txt | awk '{print $3}')
+echo "Generating Solidity Verifier..."
+delete_if_exists "src/Groth16Verifier.sol"
+snarkjs zkey export solidityverifier CDRGeneration_final.zkey src/Groth16Verifier.sol
 
-# Save performance metrics to a CSV file
-echo "ElapsedTime,MemoryUsage" > zkMetrics/groth16/performance_metrics.csv
-echo "$elapsed_time,$memory_usage" >> zkMetrics/groth16/performance_metrics.csv
+# echo "Step 13: Generating the proof and benchmarking performance..."
+# /usr/bin/time -f "Elapsed Time: %e seconds\nMaximum Memory: %M KB" snarkjs groth16 prove CDRGeneration_final.zkey witness.wtns proof.json public.json 2> zkMetrics/groth16/time_output.txt
 
-echo "Proof generation completed. Performance metrics saved to zkMetrics/groth16/performance_metrics.csv."
-echo "Elapsed Time: $elapsed_time seconds"
-echo "Maximum Memory: $memory_usage KB"
+# # Extract proving time and memory usage
+# elapsed_time=$(grep "Elapsed Time" zkMetrics/groth16/time_output.txt | awk '{print $3}')
+# memory_usage=$(grep "Maximum Memory" zkMetrics/groth16/time_output.txt | awk '{print $3}')
+
+# # Save performance metrics to a CSV file
+# echo "ElapsedTime,MemoryUsage" > zkMetrics/groth16/performance_metrics.csv
+# echo "$elapsed_time,$memory_usage" >> zkMetrics/groth16/performance_metrics.csv
+
+# echo "Proof generation completed. Performance metrics saved to zkMetrics/groth16/performance_metrics.csv."
+# echo "Elapsed Time: $elapsed_time seconds"
+# echo "Maximum Memory: $memory_usage KB"
